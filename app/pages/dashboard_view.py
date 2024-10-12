224904d2679508aa49cc02358a46afda0ade6f2e
Marius A <67149799+GitWizMarius@users.noreply.github.com>
2024-10-12 22:06:02 +0200

    Streamlit added Oil Prices in Dashboard

--- app/pages/dashboard_view.py
+++ app/pages/dashboard_view.py
@@ -1,26 +1,71 @@
 import pandas as pd
 import streamlit as st
 from streamlit_extras.grid import grid
-import plotly.graph_objects as go
+from src.api import OilPriceAPI
 
 
 def load_data(file_path: str) -> pd.DataFrame:
+    # Load data from the specified file
     data = pd.read_pickle(file_path)
     print(data.head())
 
-    # Remove all with Tank-ID = 5
+    # Remove all rows where Tank-ID = 5
     data = data[data["Tank-ID"] != 5]
 
-    # Add Oil Prices
+    # Convert PLZ to string and remove any trailing .0
+    data["PLZ"] = data["PLZ"].astype(str)
+    data["PLZ"] = data["PLZ"].str.replace(".0", "", regex=False)
+
+    # Get min and max date from the data
+    start_date = data["Zeitstempel"].min().strftime("%Y-%m-%d")
+    end_date = data["Zeitstempel"].max().strftime("%Y-%m-%d")
+
+    # Get OilPrice for each Tank-ID from OilPriceAPI
+    oil_price_api = OilPriceAPI()
+
+    # Get unique PLZ codes
+    plz = data["PLZ"].unique()
+
+    all_oil_prices = pd.DataFrame()  # Initialize an empty DataFrame for all oil prices
+    # Loop through all unique PLZ codes
+    for plz_code in plz:
+        print(plz_code)
+
+        # Fetch oil prices for the given PLZ, start date, and end date
+        oil_price_df = oil_price_api.get_heizoel(plz_code, start_date, end_date)
+
+        # Add a column for the PLZ code (optional, if you want to keep track of which PLZ each row belongs to)
+        oil_price_df["PLZ_Code"] = plz_code
+
+        # Concatenate the fetched DataFrame to the main DataFrame
+        all_oil_prices = pd.concat([all_oil_prices, oil_price_df], ignore_index=True)
+
+    # Optionally, reset the index of the final DataFrame
+    all_oil_prices.reset_index(drop=True, inplace=True)
+
+    # Rename Date to Zeitstempel
+    all_oil_prices.rename(columns={"Date": "Zeitstempel"}, inplace=True)
+    # Format to timestamp
+    all_oil_prices["Zeitstempel"] = pd.to_datetime(all_oil_prices["Zeitstempel"])
+
+    # Ensure "Zeitstempel" is of the same type in both DataFrames
+    data["Zeitstempel"] = pd.to_datetime(data["Zeitstempel"])
+    all_oil_prices["Zeitstempel"] = pd.to_datetime(all_oil_prices["Zeitstempel"])
+
+    # Merge the data with the Prices
+    data = pd.merge(data, all_oil_prices, on=["Zeitstempel", "PLZ"], how="left")
+    data["Oil Price"] = data["Price"]
 
     # Today DataSet
     today_data = data.sort_values("Zeitstempel").groupby("Tank-ID").tail(1)
-    today_data = today_data[["Tank-ID", "Füllstand", "Linear Prozentwert", "Maximale Füllgrenze", "Temperatur", "PLZ"]]
+    today_data = today_data[
+        ["Tank-ID", "Füllstand", "Linear Prozentwert", "Maximale Füllgrenze", "Temperatur", "PLZ", "Oil Price"]
+    ]
 
     # Yesterday DataSet
     yesterday_data = data.sort_values("Zeitstempel").groupby("Tank-ID").nth(-2)
     yesterday_data = yesterday_data[
-        ["Tank-ID", "Füllstand", "Linear Prozentwert", "Maximale Füllgrenze", "Temperatur", "PLZ"]
+        ["Tank-ID", "Füllstand", "Linear Prozentwert", "Maximale Füllgrenze", "Temperatur", "PLZ", "Oil Price"]
     ]
 
     return today_data, yesterday_data
@@ -100,7 +145,14 @@ def view_dashboard_page(CFG: dict) -> None:
         )
 
     with my_grid.container():
-        st.metric("Avg. Heating Oil Price:", "0.00 Euro", "1", delta_color="inverse")
+        avg_oil_price_today = filtered_data_today["Oil Price"].mean()
+        avg_oil_price_yesterday = filtered_data_yesterday["Oil Price"].mean()
+        st.metric(
+            "Avg. Heating Oil Price:",
+            f"{avg_oil_price_today:.2f} EUR",
+            f"{avg_oil_price_today - avg_oil_price_yesterday:.2f} EUR",
+            delta_color="inverse",
+        )
 
     # Row 2: Display Tank information
     with my_grid.container():
