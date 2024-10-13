55d9fe711fa4b491a1a34737cb061bbb4b9528f1
negralessio <Alessio.Negrini@live.de>
2024-10-13 10:10:36 +0200

    Misc: Added welcome text

--- app/constants/text.py
+++ app/constants/text.py
@@ -5,3 +5,20 @@ Contains Markdown Text to display in the streamlit GUI to avoid cluttering the m
 SIDEBAR_CONTENT = """
 Black Forest Hackathon 2024: Challenge 4
 """
+
+WELCOME_TEXT = """
+    >"Der Black Forest Hackathon ist der Treffpunkt für Visionäre, um ein Wochenende lang in einer einzigartigen Umgebung und Atmosphäre mit den neusten Technologien zu arbeiten.
+    Wir bringen dich mit Gleichgesinnten zusammen, damit du in nur 48 Stunden von Grund auf innovative Projekte umsetzten kannst." -- *blackforesthackathon.de*
+
+    ### Über die Challenge
+    Die VEGA Grieshaber KG bietet Kunden eine innovative Lösung zur Überwachung und Prognose ihrer Lagerbestände an, was rechtzeitige Nachbestellungen ermöglicht. Kann der Nachkaufzeitpunkt für Lagerbestände durch aktuelle, individuelle Einspeisung von Preis-, Chargen- und Materialinformationen optimiert werden?
+    Diese optimalen Nachkaufzeitpunkte sollen unter Berücksichtigung des prognostizierten Füllstandes, Preis (-prognosen) aus öffentlichen oder privaten Preisinformationen ermittelt werden.
+    Als besondere Herausforderung ist zu sehen, dass die Daten zwischen verschiedenen Kunden nicht geteilt werden dürfen.
+
+    ### Wer wir sind
+    Das Team 'Data Pilots' besteht aus vier Data Enthusiasts:
+    - Nina M.
+    - Hevin H.
+    - Marius A.
+    - Alessio N.
+    """
--- app/pages/welcome_view.py
+++ app/pages/welcome_view.py
@@ -7,8 +7,4 @@ import app.constants.text as text
 
 def view_welcome_page(CFG: dict) -> None:
     st.header(text.SIDEBAR_CONTENT)
-    st.markdown(
-        """
-    Die VEGA Grieshaber KG bietet Kunden eine innovative Lösung zur Überwachung und Prognose ihrer Lagerbestände an, was rechtzeitige Nachbestellungen ermöglicht. Kann der Nachkaufzeitpunkt für Lagerbestände durch aktuelle, individuelle Einspeisung von Preis-, Chargen- und Materialinformationen optimiert werden? Diese optimalen Nachkaufzeitpunkte sollen unter Berücksichtigung des prognostizierten Füllstandes, Preis (-prognosen) aus öffentlichen oder privaten Preisinformationen ermittelt werden. Als besondere Herausforderung ist zu sehen, dass die Daten zwischen verschiedenen Kunden nicht geteilt werden dürfen.
-    """
-    )
+    st.markdown(text.WELCOME_TEXT)
